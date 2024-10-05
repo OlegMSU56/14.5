45bff0fff4f7366b15d89a473a18f44ac4059207
Oleg <sagdeev.ol@yandex.ru>
2024-10-05 21:15:35 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,42 +1,7 @@
 import sqlite3
 
-def initiate_db2(db2_name='users.db'): #14_5
-    connection = sqlite3.connect(db2_name)
-    cursor = connection.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users(
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    age INTEGER,
-    balance INTEGER)
-    ''')
-    connection.commit()
-    connection.close()
-
-def add_user(username, email, age): # 14_5
-    connection = sqlite3.connect()  # Подключаемся к базе данных
-    cursor = connection.cursor()
-    check_user = cursor.execute('SELECT * FROM Users WHERE username = ?', (username, ))
-    if check_user.fetchone() is None:
-        cursor.execute(f'''
-    INSERT INTO Users VALUES("{username}","{email}","{age}", "{1000}")
-    ''')
-    connection.commit()
-    connection.close()
-
-
-def is_included(username): # 14_5
-    connection = sqlite3.connect('users.db')  # Подключаемся к базе данных
-    cursor = connection.cursor()
-    cursor.execute('SELECT COUNT(*) FROM Users WHERE username = ?', (username,))
-    user_exists = cursor.fetchone()[0] > 0
-    connection.close()
-    return user_exists
-
-
-def initiate_db1(db_name='products3.db'):
-    connection = sqlite3.connect(db_name)
+def initiate_db():
+    connection = sqlite3.connect('products5.db')
     cursor = connection.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
@@ -49,7 +14,43 @@ def initiate_db1(db_name='products3.db'):
     connection.commit()
     connection.close()
 
-def get_all_products(db_name='products3.db'):
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+        )
+    ''')
+    connection.commit()
+    connection.close()
+
+def add_user(username, email, age): # 14_5
+    connection = sqlite3.connect('users.db')  # Подключаемся к базе данных
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, 1000)'
+                   , (username, email, age))
+    connection.commit()
+    connection.close()
+
+
+def is_included(username): # 14_5
+    connection = sqlite3.connect('users.db')  # Подключаемся к базе данных
+    cursor = connection.cursor()
+    check_name = cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    try:
+        if check_name.fetchone()[1] == username:
+            return True
+    except TypeError:
+        return False
+    finally:
+        connection.close()
+
+
+def get_all_products(db_name='products5.db'):
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM Products')
@@ -58,7 +59,7 @@ def get_all_products(db_name='products3.db'):
 
     return products
 
-def add_product(title, description, price, db_name='products3.db'):
+def add_product(title, description, price, db_name='products5.db'):
     connection = sqlite3.connect(db_name) # Подключаемся к базе данных
     cursor = connection.cursor()
     cursor.execute('''
@@ -68,8 +69,7 @@ def add_product(title, description, price, db_name='products3.db'):
     connection.close()
 
 if __name__ == "__main__":
-    initiate_db2()
-    initiate_db1()
+    initiate_db()
     add_product('Продукт 1', 'Описание продукта 1', 100)
     add_product('Продукт 2', 'Описание продукта 2', 200)
     add_product('Продукт 3', 'Описание продукта 3', 300)
